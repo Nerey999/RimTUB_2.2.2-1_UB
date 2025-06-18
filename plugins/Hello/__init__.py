7ab303689cfe4412ad7f510797fd157537ab03bd
Yasoslav <saburovyar.2008@mail.ru>
2025-06-18 21:40:40 +0400

    delete hello plugin

--- plugins/Hello/__init__.py
+++
@@ -1,9 +0,0 @@
-from utils import *
-
-async def main(app: Client, mod: Module):
-
-    cmd = mod.cmd
-        
-    @cmd('hello')
-    async def _hello(_, msg: M):
-        await msg.edit("ПРИВЕЕЕТ!!!")
